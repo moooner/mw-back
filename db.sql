efc153e46bad02bfbcd677c6a955b9f7dac84762
dhwlgjs13 <dhwlgjs13@gmail.com>
2021-05-02 16:27:17 +0900

    🐛 revise base on comments

--- db.sql
+++ db.sql
@@ -6,23 +6,23 @@ USE login_db;
 drop table if exists users;
 create table if not exists users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
-    google_user_id VARCHAR(32) NOT NULL,
-    apple_user_id VARCHAR(32) NOT NULL,
-    email CHAR(50) NOT NULL,
-    customized_value FLOAT
+    google_user_id VARCHAR(32) NULL,
+    apple_user_id VARCHAR(32) NULL,
+    email CHAR(50) NULL,
+    customized_value FLOAT default(0)
 ) ENGINE = INNODB;
 
 drop table if exists customized_value_logs;
 create table if not exists customized_value_logs(
-    log_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id int,
+    log_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_id int NOT NULL,
     question_id VARCHAR(32) NOT NULL,
-    answer int
+    answer boolean
 );
 
 drop table if exists questions;
-create table if not exists queuser_idstions(
+create table if not exists questions(
     question_id INT PRIMARY KEY AUTO_INCREMENT,
-    question_text VARCHAR(100),
-    delta FLOAT
+    question_text TEXT NOT NULL,
+    delta FLOAT NOT NULL
 );
