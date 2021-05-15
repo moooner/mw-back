0703ff5b304a0949e5583f7d37554871814c65db
racoon66 <dhwlgjs13@naver.com>
2021-05-15 18:28:19 +0900

    Update main.go
    Co-authored-by: Hack <hgkim@gnmpm.com>

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "user:pass@tcp(127.0.0.1:3306)/mw?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
